e45839fe131a248b0661f34b75acb573318f7c8a
ptrBodnar <peterbodnar1213@gmail.com>
2019-07-16 20:35:19 +0300

    separated js code into main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var map = L.map('map').setView([51, 23], 4);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.light'
+	}).addTo(map);
+
+
+	d3.json("all_tvo.geojson").then(function(data) {
+	  	var geojson;
+
+
+		geojson = L.geoJson(data, {
+			// style: style,
+			// onEachFeature: onEachFeature
+		}).addTo(map);
+		});
